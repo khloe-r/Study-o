96b188d03ad54493e4bd176d6e13b03f0a329bc5
Sia Bhatia <bhatiasia@gmail.com>
2021-08-03 03:38:51 +0000

    Added calendar view - need to change it so it is for user

--- public/js/calendarView.js
+++ public/js/calendarView.js
@@ -1,1 +1,104 @@
 console.log("script running");
+// date variables
+var now = new Date();
+today = now.toISOString();
+
+var twoHoursLater = new Date(now.getTime() + (2 * 1000 * 60 * 60));
+twoHoursLater = twoHoursLater.toISOString();
+
+// google api console clientID and apiKey (https://code.google.com/apis/console/#project:568391772772)
+var clientId = '243003109957-8bpj74e6eib10r888ccleeh1t0lhqp8g.apps.googleusercontent.com';
+var apiKey = 'AIzaSyDEL7uevoZ_rQ3JLHWDTqHGKrWvTRG-tKY';
+
+// enter the scope of current project (this API must be turned on in the google console)
+var scopes = 'https://www.googleapis.com/auth/calendar';
+
+
+// Oauth2 functions
+function handleClientLoad() {
+    gapi.client.setApiKey(apiKey);
+    window.setTimeout(checkAuth, 1);
+}
+
+function checkAuth() {
+    gapi.auth.authorize({ client_id: clientId, scope: scopes, immediate: true }, handleAuthResult);
+}
+
+// show/hide the 'authorize' button, depending on application state
+function handleAuthResult(authResult) {
+    var authorizeButton = document.getElementById('authorize-button');
+    var resultPanel = document.getElementById('result-panel');
+    var resultTitle = document.getElementById('result-title');
+    console.log(authResult);
+
+    if (authResult.error) {
+        location.replace("https://study-o.web.app/gCal.html");
+
+        //makeApiCall();	
+    } //else {													// otherwise, show button
+        // authorizeButton.style.visibility = 'visible';
+        // resultPanel.className += ' panel-danger';				// make panel red
+        // authorizeButton.onclick = handleAuthClick;				// setup function to handle button click
+    //}
+}
+
+
+//function triggered when user authorizes app
+function handleAuthClick(event) {
+    gapi.auth.authorize({ client_id: clientId, scope: scopes, immediate: false }, handleAuthResult);
+    return false;
+}
+
+// setup event details
+
+var resource = {
+    
+    "summary": "Sample Event " + Math.floor((Math.random() * 10) + 1),
+    "start": {
+        "dateTime": today
+    },
+    "end": {
+        "dateTime": twoHoursLater
+    },
+    "description": "test"
+};
+
+// function load the calendar api and make the api call
+function makeApiCall() {
+    gapi.client.load('calendar', 'v3', function () {					// load the calendar api (version 3)
+        var request = gapi.client.calendar.events.insert({
+            'calendarId': 'primary',	// calendar ID
+            "resource": resource							// pass event details with api call
+        });
+
+        gapi.client.calendar.events.list({
+            "calendarId": "primary"
+        })
+            .then(function (response) {
+                // Handle the results here (response.result has the parsed body).
+                console.log("Response", response);
+            },
+                function (err) { console.error("Execute error", err); });
+
+        // handle the response from our api call
+        request.execute(function (resp, errr) {
+            if (resp.status == 'confirmed') {
+                //document.getElementById('event-response').innerHTML = "Event created successfully. View it <a href='" + resp.htmlLink + "'>online here</a>.";
+                console.log("successfully submitted!");
+                alert("it submitted!");
+            } else {
+                console.log(errr);
+                //document.getElementById('event-response').innerHTML = "There was a problem. Reload page and try again.";
+                // calendarList.get('primary')
+            }
+
+
+            console.log(resp);
+        });
+    });
+}
+// let buttonSubmit = document.querySelector("#submitButton");
+// buttonSubmit.addEventListener("click", function() {
+//   makeApiCall();
+// });
+
