5ec3363fe5f9fbdb01e33d732eaf3001bfdf6dfb
Sia Bhatia <bhatiasia@gmail.com>
2021-08-03 19:49:56 +0000

    Got one part of form workinggit add .

--- public/js/calendarView.js
+++ public/js/calendarView.js
@@ -36,9 +36,9 @@ function handleAuthResult(authResult) {
 
         //makeApiCall();	
     } //else {													// otherwise, show button
-        // authorizeButton.style.visibility = 'visible';
-        // resultPanel.className += ' panel-danger';				// make panel red
-        // authorizeButton.onclick = handleAuthClick;				// setup function to handle button click
+    // authorizeButton.style.visibility = 'visible';
+    // resultPanel.className += ' panel-danger';				// make panel red
+    // authorizeButton.onclick = handleAuthClick;				// setup function to handle button click
     //}
 }
 
@@ -51,51 +51,67 @@ function handleAuthClick(event) {
 
 // setup event details
 
-var resource = {
-    
-    "summary": "Sample Event " + Math.floor((Math.random() * 10) + 1),
-    "start": {
-        "dateTime": today
-    },
-    "end": {
-        "dateTime": twoHoursLater
-    },
-    "description": "test"
-};
+// var resource = {
+
+//     "summary": "Sample Event " + Math.floor((Math.random() * 10) + 1),
+//     "start": {
+//         "dateTime": today
+//     },
+//     "end": {
+//         "dateTime": twoHoursLater
+//     },
+//     "description": "test"
+// };
 
 // function load the calendar api and make the api call
 function makeApiCall() {
-    gapi.client.load('calendar', 'v3', function () {					// load the calendar api (version 3)
-        var request = gapi.client.calendar.events.insert({
-            'calendarId': 'primary',	// calendar ID
-            "resource": resource							// pass event details with api call
+    if (document.querySelector("#dueDateInput").value.length !== 0 && document.querySelector("#titleInput").value.length !== 0 && document.querySelector("#dueTimeInput").value.length !== 0) {
+        gapi.client.load('calendar', 'v3', function () {					// load the calendar api (version 3)
+            var request = gapi.client.calendar.events.insert({
+                'calendarId': 'primary',	// calendar ID
+                "resource": {
+                    "end": {
+                        "date": "2021-08-04"
+                    },
+                    "start": {
+                        "date": "2021-08-03"
+                    },
+                    "description": "hello",
+                    "summary":document.querySelector("#titleInput").value
+                }
+            });
+
+            gapi.client.calendar.events.list({
+                "calendarId": "primary"
+            })
+                .then(function (response) {
+                    // Handle the results here (response.result has the parsed body).
+                    console.log("Response", response);
+                },
+                    function (err) { console.error("Execute error", err); });
+
+            // handle the response from our api call
+            request.execute(function (resp, errr) {
+                if (resp.status == 'confirmed') {
+                    //document.getElementById('event-response').innerHTML = "Event created successfully. View it <a href='" + resp.htmlLink + "'>online here</a>.";
+                    console.log("successfully submitted!");
+                    alert("it submitted!");
+                } else {
+                    console.log(errr);
+                    //document.getElementById('event-response').innerHTML = "There was a problem. Reload page and try again.";
+                    // calendarList.get('primary')
+                }
+
+
+                console.log(resp);
+            });
         });
 
-        gapi.client.calendar.events.list({
-            "calendarId": "primary"
-        })
-            .then(function (response) {
-                // Handle the results here (response.result has the parsed body).
-                console.log("Response", response);
-            },
-                function (err) { console.error("Execute error", err); });
+    }
+    else {
+        alert("Fill in all fields");
+    }
 
-        // handle the response from our api call
-        request.execute(function (resp, errr) {
-            if (resp.status == 'confirmed') {
-                //document.getElementById('event-response').innerHTML = "Event created successfully. View it <a href='" + resp.htmlLink + "'>online here</a>.";
-                console.log("successfully submitted!");
-                alert("it submitted!");
-            } else {
-                console.log(errr);
-                //document.getElementById('event-response').innerHTML = "There was a problem. Reload page and try again.";
-                // calendarList.get('primary')
-            }
-
-
-            console.log(resp);
-        });
-    });
 }
 // let buttonSubmit = document.querySelector("#submitButton");
 // buttonSubmit.addEventListener("click", function() {
